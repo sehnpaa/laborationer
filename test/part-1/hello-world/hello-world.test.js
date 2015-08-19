ed723053689f51a3fcd4a8088d716c67de3d8f63
Leitet <johan.leitet@lnu.se>
2015-08-19 11:07:40 +0200

    Initial Hello World

---
+++ test/part-1/hello-world/hello-world.test.js
@@ -0,0 +1,12 @@
+var hello = require("../src/hello-world");
+var expect = require("chai").expect;
+
+describe("Test the function sayHello", function() {
+    "use strict";
+
+    it("sayHello(); should return \"Hello, World!\"", function(done) {
+        expect(hello.sayHello()).to.eql("Hello, World!");
+        done();
+    });
+
+});
