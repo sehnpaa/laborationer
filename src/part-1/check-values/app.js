b3b608a5e76eb21c9059fc6e21f18636d9454c0b
John Häggerud <john.haggerud@lnu.se>
2015-09-01 18:17:07 +0200

    inital branch commit

---
+++ src/part-1/check-values/app.js
@@ -0,0 +1,9 @@
+// run app with 'npm start'
+// run tests with 'npm test'
+
+var vc = require("./value-checker");
+
+// Test the sum-function here
+
+// Print the result to the terminal (console)
+console.log(vc.check(true));
---
+++ src/part-1/check-values/check-values.js
@@ -0,0 +1,24 @@
+/**
+ * Module for a simple value checker
+ *
+ * @author thajo
+ * @version 1.0.0
+ */
+
+"use strict";
+
+/**
+ * Sends back a string with a message depending on what the parameters
+ * data type.
+ * See the tests at /test/part-1/check-values/ for more information how to respond
+ * @param {*} value - The variable to exam
+ * @returns {string} - A message that tells what the data type is (according to javascript).
+ * See readme
+ */
+exports.check = function(value) {
+    var startMessage = "You send me ";
+    // your code here
+
+
+    return startMessage;
+};
