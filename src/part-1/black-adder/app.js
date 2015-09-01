789bb5ebf60596774db923e1fef7355ad913a009
John Häggerud <john.haggerud@lnu.se>
2015-09-01 18:21:13 +0200

    inital branch commit

---
+++ src/part-1/black-adder/app.js
@@ -0,0 +1,9 @@
+// run tests with 'npm test'
+
+var sc = require("./simple-calculator");
+var result = sc.add(1, 2);
+
+// Test the sum-function here
+
+// Print the result to the terminal (console)
+console.log(result);
---
+++ src/part-1/black-adder/black-adder.js
@@ -0,0 +1,12 @@
+/**
+ * Module for a simple calculator
+ *
+ * @author thajo
+ * @version 1.0.0
+ */
+
+"use strict";
+
+exports.add = function() {
+    // Your code here
+};
